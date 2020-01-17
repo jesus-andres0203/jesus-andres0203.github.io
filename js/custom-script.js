a597eab24efb4c6095db52b17baf855a7d827247
Andres <jesus.andres0203@gmail.com>
2020-01-17 10:57:32 -0800

    Coming soon update

---
+++ js/custom-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function($){
+    $('body').css('background-image','url(img/mar-city.jpg)');
+});
+$(window).bind("load", function(){
+    $('body').css('background-image','url(img/mar-city.jpg)');
+});
--- js/main.js
+++ js/main.js
@@ -1,27 +1,79 @@
-var callback = function(){
-	$('.item-skills').each(function(){
-		newWidth = $(this).parent().width() * $(this).data('percent');
-		$(this).width(0);
-    $(this).animate({
-        width: newWidth,
-    }, 1000);
-	});
-	$('.icons-red').each(function(){
-		height = $(this).height();
-    $(this).animate({
-        height: 14,
-    }, 2000);
-	});
-};
-$(document).ready(callback);
 
-var resize;
-var box = jQuery('.box');
-if(box.length > 0){
-	window.onresize = function() {
-		clearTimeout(resize);
-		resize = setTimeout(function(){
-			callback();
-		}, 100);
-	};
-}
+(function ($) {
+    "use strict";
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+    
+    
+   /*==================================================================
+    [ Simple slide100 ]*/
+
+    $('.simpleslide100').each(function(){
+        var delay = 7000;
+        var speed = 1000;
+        var itemSlide = $(this).find('.simpleslide100-item');
+        var nowSlide = 0;
+
+        $(itemSlide).hide();
+        $(itemSlide[nowSlide]).show();
+        nowSlide++;
+        if(nowSlide >= itemSlide.length) {nowSlide = 0;}
+
+        setInterval(function(){
+            $(itemSlide).fadeOut(speed);
+            $(itemSlide[nowSlide]).fadeIn(speed);
+            nowSlide++;
+            if(nowSlide >= itemSlide.length) {nowSlide = 0;}
+        },delay);
+    });
+
+
+})(jQuery);
